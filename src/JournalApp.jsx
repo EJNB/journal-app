cbcb5dee39bd211f76c869a3156fbdcbc010e0f4
javier <javiernieto1989@gmail.com>
2022-10-19 09:18:25 -0400

    Make all components and pages

---
+++ src/JournalApp.jsx
@@ -0,0 +1,10 @@
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
+
+export default function JournalApp() {
+  return (
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
+  );
+}
---
+++ src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,55 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
+export function RegisterPage() {
+  return (
+    <AuthLayout title="Crear cuenta">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Javier Nieto"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contrasena"
+              type="password"
+              placeholder="Contrasena"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear Cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} to="/auth/login" color="inherit">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+}
